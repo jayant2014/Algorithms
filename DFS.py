8783d4210c3cd6221c03b91a5dea6228feff5ca1
Jayant Parida <jayant2014@users.noreply.github.com>
2020-04-07 20:20:27 +0530

    Update DFS.py

--- DFS.py
+++ DFS.py
@@ -1,1 +1,15 @@
 
+class Node:
+    def __init__(self, name):
+        self.children = []
+        self.name = name
+
+    def add_child(self, name):
+        self.children.append(Node(name))
+        return self
+
+    def depth_first_search(self, array):
+        array.append(self.name)
+        for child in self.children:
+            child.depth_first_search(array)
+        return array
